c110f0cfe9c2d4df81095917662dac0e56c50d3c
Asiarv25 <67976040+Asiarv25@users.noreply.github.com>
2020-08-05 14:54:52 -0500

    Add files via upload

---
+++ Asia_R_ComicBookExercise/Asia_R_ComicBookExercise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Asia_R_ComicBookExercise
+//
+//  Created by Asia Reid on 7/29/20.
+//  Copyright © 2020 Asia Reid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
